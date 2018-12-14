af84c7ef4448dd9a1bd70d91a1948b54f692d782
Admin <admin@192.168.91.128>
2018-12-14 17:06:45 +0300

    Add new item functionality complete

--- Todoy/TodoyViewController.swift
+++ Todoy/TodoyViewController.swift
@@ -10,18 +10,19 @@ import UIKit
 
 class TodoyViewController: UITableViewController {
 
-    let itemArray = ["mmm" , "eee"  , "sss"]
+    var itemArray = ["mmm" , "eee"  , "sss"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
      }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
 
@@ -40,8 +41,36 @@ class TodoyViewController: UITableViewController {
         }
         
 
-    
+    // just for animation while clicking on the text.
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //ADD New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            //what happen when ser clicks
+            
+            self.itemArray.append(textField.text!)
+              self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Creat new item"
+            print(alertTextField.text)
+            textField = alertTextField
+        }
+        
+        
+        alert.addAction(action)
+        
+        present(alert , animated: true , completion: nil)
+
     }
     
   
